0b2c029f5654b0f3861743c9d7a84227fa4e5ad4
Dan Arthur <danielarthur99@hotmail.co.uk>
2024-03-17 19:10:40 +0000

    feat:arrow-functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,38 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Arrow Function With Parameters
+
+const AddTwoNumbers2 = (a, b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers3 = (a, b) => a + b;
+let sum3 = addTwoNumbers3(3, 5);
+console.log(sum3);
+
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("Hello There!!");
+
+const sayHello = () => console.log("hello");
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multi-line string!
+    </p>`
+);
+console.log(returnMultipleLines());
